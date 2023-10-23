acd2b0f5f8b67710588efb60c70a093cf7e30441
onurkaygn <92956138+onurkaygn@users.noreply.github.com>
2023-10-23 17:33:50 +0300

    tüm ödevler yüklendi

---
+++ KoleksiyonAlgoritmasi2/KoleksiyonAlgoritmasi2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace KoleksiyonAlgoritma1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            int[] inputArray = new int[20];
+            Console.WriteLine("20 adet sayı giriniz...");
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine((i + 1) + ". sayıyı giriniz...");
+                input = Convert.ToInt32(Console.ReadLine());
+                inputArray[i] = input;
+            }
+
+            
+            Array.Sort(inputArray);
+            int smallArithmetic = (inputArray[0] + inputArray[1] + inputArray[2])/3;
+            int bigArithmetic = (inputArray[inputArray.Length - 1] + inputArray[inputArray.Length - 2] + inputArray[inputArray.Length - 3]) / 3;
+
+            Console.WriteLine("En küçük 3 sayı: "+inputArray[0] +" "+ inputArray[1] +" "+ inputArray[2]);
+            Console.WriteLine("En küçük 3 sayının aritmetik ortalaması: " + smallArithmetic);
+            Console.WriteLine("En büyük 3 sayı: "+inputArray[inputArray.Length-1] + " " + inputArray[inputArray.Length-2] + " " + inputArray[inputArray.Length-3]);
+            Console.WriteLine("En büyük 3 sayının aritmetik ortalaması: " + bigArithmetic);
+
+
+
+        }
+    }
+}
---
+++ KoleksiyonAlgoritmasi3/KoleksiyonAlgoritmasi3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+namespace KoleksiyonAlgoritma1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            string input = Console.ReadLine();
+            string inputLower = input.ToLower();
+            ArrayList sesliHarfler = new ArrayList();
+
+           foreach (var item in inputLower) { 
+                if(item == 'a' || item == 'e' || item == 'ı' || item == 'i' || item == 'o' || item == 'ö' || item == 'u' || item == 'ü')
+                {
+                    sesliHarfler.Add(item);
+                }
+            }
+           foreach (var harfler in sesliHarfler)
+            {
+                Console.WriteLine(harfler);
+            }
+
+            }
+    }
+    }
